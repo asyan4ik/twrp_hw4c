cddc04aef3beddfe7a085b5d8e7faaa679e6e6c1
Vladimir <acbkaa@gmail.com>
2016-03-21 21:30:46 +0300

    update twrp 3.0

--- device_hw4c.mk
+++ device_hw4c.mk
@@ -13,15 +13,6 @@ endif
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
-    
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/system/bin/linker:recovery/root/system/bin/linker \
-    $(LOCAL_PATH)/recovery/system/lib/libc.so:recovery/root/system/lib/libc.so \
-    $(LOCAL_PATH)/recovery/system/lib/libdl.so:recovery/root/system/lib/libdl.so \
-    $(LOCAL_PATH)/recovery/system/lib/liblog.so:recovery/root/system/lib/liblog.so \
-    $(LOCAL_PATH)/recovery/system/lib/libm.so:recovery/root/system/lib/libm.so \
-    $(LOCAL_PATH)/recovery/system/lib/libstdc++.so:recovery/root/system/lib/libstdc++.so \
-    $(LOCAL_PATH)/recovery/system/lib/libz.so:recovery/root/system/lib/libz.so
 
 $(call inherit-product, build/target/product/full.mk)
 
